dcd1626f670d4deb621dd7246b6e36b959a5ba0a
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2021-06-29 21:55:50 -0700

    Implemented Seven Movement Animations
    Implemented animations for flying to the right, left, neutral, and when on the ground (not currently visible).
    
    Note: The transitions between the animations are currently a bit jarring, might have to adjust them later

--- SevenToHeaven/Assets/Scripts/PlayerMovement.cs
+++ SevenToHeaven/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
@@ -10,9 +11,16 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rb2d;
     private bool windIsBlowing;
 
+    private Animator movementAnimator;
+    [SerializeField]
+    private SpriteRenderer sevenSprite;
+    private List<int> animatorParameterIds = new List<int>();
 
     private void Start() { // Initialize variable
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        movementAnimator = gameObject.GetComponent<Animator>();
+        animatorParameterIds.Add(Animator.StringToHash("IsInAir"));
+        animatorParameterIds.Add(Animator.StringToHash("MovingHorizontally"));
     }
 
     private void Update() {
@@ -22,6 +30,18 @@ public class PlayerMovement : MonoBehaviour {
             direction = (mousePos - transform.position).normalized;
         } else
             windIsBlowing = false;
+        if (rb2d.velocity.x > -0.5 && rb2d.velocity.x < 0.5) {
+            movementAnimator.SetBool(animatorParameterIds[1], false);
+        }
+        else {
+            movementAnimator.SetBool(animatorParameterIds[1], true);
+            if (rb2d.velocity.x > 0) {
+                sevenSprite.flipX = false;
+            }
+            else {
+                sevenSprite.flipX = true;
+            }
+        }
     }
 
     private void FixedUpdate() {
