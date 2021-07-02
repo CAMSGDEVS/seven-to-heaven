07c44c8c199f7d7944e275ab16795958606e32e0
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-07-01 22:49:56 -0700

    Added cursor sprite changing
    Change the cursor sprite based on its angle.
    Imported sprite textures into the resources folder.

--- SevenToHeaven/Assets/Scripts/CursorSprite.cs
+++ SevenToHeaven/Assets/Scripts/CursorSprite.cs
@@ -4,20 +4,37 @@ public class CursorSprite : MonoBehaviour {
 
     public GameObject player;
     public GameObject cursorSprite;
+    public SpriteRenderer sRenderer;
 
     private float angle;
     private Vector3 mousePos;
+    private Object[] cursorSprites;
+    private Sprite sprite;
 
     private void Start() {
+        sRenderer = cursorSprite.GetComponent<SpriteRenderer>();
+        cursorSprites = Resources.LoadAll("Cursor", typeof(Sprite));
         Cursor.visible = false;
     }
 
     private void Update() {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cursorSprite.transform.position = mousePos + new Vector3(0, 0, 10);
-
         angle = Mathf.Atan2(player.transform.position.y - mousePos.y, player.transform.position.x - mousePos.x) * Mathf.Rad2Deg;
-        cursorSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if (angle > -90f && angle < 90f) {
+            sprite = (Sprite)cursorSprites[(int)Mathf.Abs(angle / 22.5f) % 4];
+            sRenderer.flipX = false;
+        } else {
+            sprite = (Sprite)cursorSprites[3 - (int)Mathf.Abs(angle / 22.5f) % 4];
+            sRenderer.flipX = true;
+        }
+        if (angle > 0)
+            sRenderer.flipY = false;
+        else
+            sRenderer.flipY = true;
+
+        sRenderer.sprite = sprite;
     }
 
 }
