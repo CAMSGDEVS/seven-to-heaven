fb7ea98bc21bf95f71010f24b119fd46f8efb798
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2021-07-01 21:08:50 -0700

    Update Player Movement
    Moved animation to a separate class
    Made class a singleton

--- SevenToHeaven/Assets/Scripts/PlayerMovement.cs
+++ SevenToHeaven/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    private PlayerMovement _instance;
+    public PlayerMovement Instance {
+        get {
+            if (_instance == null) {
+                Debug.LogError("PlayerMovement is null");
+            }
+            return _instance;
+        } set {}
+    }
 
     public float windStrength = 5f;
 
@@ -11,17 +20,15 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rb2d;
     private bool windIsBlowing;
 
-    private Animator movementAnimator;
-    [SerializeField]
-    private SpriteRenderer sevenSprite;
-    private List<int> animatorParameterIds = new List<int>();
+    public bool onGround;
+    private void Awake() {
+        _instance = this;
+    }
 
     private void Start() { // Initialize variable
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
-        movementAnimator = gameObject.GetComponent<Animator>();
-        animatorParameterIds.Add(Animator.StringToHash("IsInAir"));
-        animatorParameterIds.Add(Animator.StringToHash("MovingHorizontally"));
+        
     }
 
     private void Update() {
@@ -31,18 +38,6 @@ public class PlayerMovement : MonoBehaviour {
             direction = (mousePos - transform.position).normalized;
         } else
             windIsBlowing = false;
-        if (rb2d.velocity.x > -0.5 && rb2d.velocity.x < 0.5) {
-            movementAnimator.SetBool(animatorParameterIds[1], false);
-        }
-        else {
-            movementAnimator.SetBool(animatorParameterIds[1], true);
-            if (rb2d.velocity.x > 0) {
-                sevenSprite.flipX = false;
-            }
-            else {
-                sevenSprite.flipX = true;
-            }
-        }
     }
 
     private void FixedUpdate() {
@@ -50,5 +45,4 @@ public class PlayerMovement : MonoBehaviour {
             rb2d.velocity = new Vector2(direction.x * -1 * windStrength, direction.y * -1 * windStrength);
         }
     }
-
 }
