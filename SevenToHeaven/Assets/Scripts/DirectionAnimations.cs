7158a7d242ff3a6bd837192681ae37a16067657e
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2021-07-04 13:56:27 -0700

    Import direction animations from animation-test branch
    from commit 67a0457b7352d482d5c57519677246f85a710175
    Switched to using a script instead of the built-in animations
    Imported images
    Currently EXTREMELY inefficient, may revert back to using animations
    Hard-coded the animations using lists, likely will switch to encoding in an xml file later as this is extremely tedious and inefficient
    The animation from 115 to 75 degrees is broken, and the inverse animation is functional but not good

---
+++ SevenToHeaven/Assets/Scripts/DirectionAnimations.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DirectionAnimations : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private GameObject sevenGameObject;
+    private SpriteRenderer sevenSpriteR;
+
+    private List<Sprite> sevenFlyingSprites = new List<Sprite>();
+    private List<Sprite> sevenFlyingTransitionSprites = new List<Sprite>();
+
+    private List<Sprite> sevenNeutralSprites = new List<Sprite>();
+    private List<int> sevenNeutralSpriteIndices = new List<int>();
+    private List<float> sevenNeutralVerticalSpriteTranslations = new List<float>();
+    private List<float> sevenNeutralHorizontalSpriteTranslations = new List<float>();
+
+    private List<Sprite> sevenIdleSprites = new List<Sprite>();
+    private List<int> sevenIdleSpriteIndices = new List<int>();
+
+    private float previousAngle = 0;
+    private int transitionFrames = 0;
+    private int animationCycle = 0;
+    private int previousAnimationType = -1;
+    private int frameCycle = 0;
+
+    private bool onGround = false;
+
+    private void Awake() {
+        sevenSpriteR = sevenGameObject.GetComponent<SpriteRenderer>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        for (int i = 0; i < 8; i++) {
+            if (i != 6) {
+                sevenFlyingSprites.Add(Resources.Load<Sprite>("Seven/Moving/seven_" + System.Convert.ToString(i * 15)));
+            } else {
+                sevenFlyingSprites.Add(null);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            sevenNeutralSprites.Add(Resources.Load<Sprite>("Seven/Neutral/seven_drop_anim_frame_0" + System.Convert.ToString(i + 1)));
+        }
+        for (int i = 0; i < 4; i++) {
+            sevenIdleSprites.Add(Resources.Load<Sprite>("Seven/Idle/seven_idle_" + System.Convert.ToString(i + 1)));
+        }
+        sevenFlyingTransitionSprites.Add(Resources.Load<Sprite>("Seven/Moving/seven_go_down"));
+        sevenFlyingTransitionSprites.Add(Resources.Load<Sprite>("Seven/Moving/seven_back_up"));
+        sevenNeutralSpriteIndices.AddRange(Enumerable.Repeat(0, 7)
+                                 .Concat(Enumerable.Repeat(1, 6)
+                                 .Concat(Enumerable.Repeat(2, 10)
+                                 .Concat(Enumerable.Repeat(1, 7)
+                                 .Concat(new[] { 0 })))).ToList());
+        sevenNeutralVerticalSpriteTranslations.AddRange(Enumerable.Repeat(0f, 7).ToList()
+                                      .Concat(Enumerable.Repeat(-1/32f, 6).ToList()
+                                      .Concat(Enumerable.Repeat(-1/16f, 10).ToList()
+                                      .Concat(Enumerable.Repeat(-1/32f, 7).ToList()
+                                      .Concat(new[] { 0f })))));
+        sevenNeutralHorizontalSpriteTranslations.AddRange(Enumerable.Repeat(0f, 13).ToList()
+                                      .Concat(Enumerable.Repeat(1 / 16f, 10).ToList()
+                                      .Concat(Enumerable.Repeat(0f, 8).ToList())));
+        sevenIdleSpriteIndices.AddRange(Enumerable.Repeat(0, 6)
+                              .Concat(Enumerable.Repeat(1, 4)
+                              .Concat(Enumerable.Repeat(2, 4)
+                              .Concat(Enumerable.Repeat(3, 8)
+                              .Concat(Enumerable.Repeat(2, 4)
+                              .Concat(Enumerable.Repeat(1, 6)
+                              .Concat(new[] { 0 })))))).ToList());
+                                      
+        
+    }
+    private void FixedUpdate() {
+        Debug.Log(rb2d.velocity);
+        frameCycle = (frameCycle + 1) % 2;
+        if (frameCycle == 1) {
+            onGround = PlayerMovement.Instance.onGround;
+            if (!onGround) {
+                if (rb2d.velocity.x > -1 && rb2d.velocity.x < 1) {
+                    NeutralAnimation();
+                } else {
+                    AngledAnimation();
+                }
+            } else {
+                IdleAnimation();
+            }
+        }
+    }
+    private void NeutralAnimation() {
+        if (previousAnimationType != 0) {
+            previousAnimationType = 0;
+            animationCycle = 0;
+        }
+        sevenGameObject.transform.localPosition = new Vector3(sevenNeutralHorizontalSpriteTranslations[animationCycle], sevenNeutralVerticalSpriteTranslations[animationCycle]);
+        sevenSpriteR.sprite = sevenNeutralSprites[sevenNeutralSpriteIndices[animationCycle]];
+        animationCycle = (animationCycle + 1) % 31;
+    }
+    private void IdleAnimation() {
+        if (previousAnimationType != 1) {
+            previousAnimationType = 1;
+            animationCycle = 0;
+        }
+        sevenSpriteR.sprite = sevenIdleSprites[sevenIdleSpriteIndices[animationCycle]];
+        animationCycle = (animationCycle + 1) % 33;
+    }
+    private void AngledAnimation() {
+        previousAnimationType = 2;
+        float angle = Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x)*180/(Mathf.PI);
+        if (angle > 90) angle = -1 * angle + 180; // Maps from 2nd quadrant to 1st quadrant (90 to 180) -> (90 to 0)
+        else if (angle < -90) angle = (-1 * angle - 180); // Maps from 3rd quadrant to 4th quadrant (-90 to -180) -> (-90 to 0)
+        angle = -1 * angle + 90; //Maps from (90 to -90) -> (0 to 180)
+
+        //Flips sprite when heading left
+        if (rb2d.velocity.x > 0) { 
+            sevenSpriteR.flipX = false;
+        } else {
+            sevenSpriteR.flipX = true;
+        }
+        int spriteIndex = AngleToIndex(angle);
+        if (spriteIndex == 7) {
+            sevenSpriteR.sprite = sevenFlyingSprites[7];
+        } else if (spriteIndex == 6 && transitionFrames < 5) {
+            if (AngleToIndex(previousAngle) < 6) {
+                sevenSpriteR.sprite = sevenFlyingTransitionSprites[1];
+            } else {
+                sevenSpriteR.sprite = sevenFlyingTransitionSprites[0];
+            }
+            transitionFrames++;
+        } else {
+            if (frameCycle == 0) {
+                if (spriteIndex == 6) {
+                    sevenSpriteR.sprite = sevenFlyingSprites[7];
+                } else {
+                    sevenSpriteR.sprite = sevenFlyingSprites[spriteIndex];
+                }
+            } else {
+                if (spriteIndex == 6) {
+                    sevenSpriteR.sprite = sevenFlyingSprites[7];
+                } else if (AngleToIndex(previousAngle) >= angle && spriteIndex < 6) {
+                    sevenSpriteR.sprite = sevenFlyingSprites[spriteIndex + 1];
+                } else if (AngleToIndex(previousAngle) < angle && spriteIndex > 0) {
+                    sevenSpriteR.sprite = sevenFlyingSprites[spriteIndex - 1];
+                } else {
+                    sevenSpriteR.sprite = sevenFlyingSprites[spriteIndex];
+                }
+            }
+            if (spriteIndex != 6) {
+                transitionFrames = 0;
+            }
+        }
+        if (angle != previousAngle) {
+            previousAngle = angle;
+        }
+    }
+
+    private int AngleToIndex(float angle) {
+        int index = (int) Mathf.Floor(angle / 15);
+        if (index == 6) {
+            index = 5;
+        } else if (index > 6 && index < 9) {
+            index = 6;
+        } else if (index > 8) {
+            index = 7;
+        }
+        return index;
+    }
+
+}
