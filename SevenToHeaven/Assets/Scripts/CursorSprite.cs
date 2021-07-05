1a5f07233cf0f6d25df94ffaf9c0d0984fcc95f7
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-07-05 13:05:26 -0700

    Update cursor sprite changing
    Imported all frames as Texture2Ds. (Alternative was to iterate through each pixel to mirror the textures).

--- SevenToHeaven/Assets/Scripts/CursorSprite.cs
+++ SevenToHeaven/Assets/Scripts/CursorSprite.cs
@@ -3,50 +3,31 @@ using UnityEngine;
 public class CursorSprite : MonoBehaviour {
 
     [SerializeField]
-    private GameObject player, cursorSprite;
+    private GameObject player;
 
     private float angle;
-    private Sprite sprite;
-    private int spriteIndex;
+    private Texture2D texture;
+    private int textureIndex;
     private Vector3 mousePos;
-    private Object[] cursorSprites;
-    private SpriteRenderer sRenderer;
+    private Object[] cursorTextures, cursorTexturesFlipped;
 
-    private void Awake() { // Initialize variables
-        sRenderer = cursorSprite.GetComponent<SpriteRenderer>();
-        cursorSprites = Resources.LoadAll("Cursor", typeof(Sprite));
-        Cursor.visible = false;
+    private void Awake() {
+        cursorTextures = Resources.LoadAll("Cursor", typeof(Texture2D));
+        cursorTexturesFlipped = Resources.LoadAll("CursorFlipped", typeof(Texture2D));
     }
 
     private void Update() {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursorSprite.transform.position = mousePos + new Vector3(0, 0, 10);
         angle = Mathf.Atan2(player.transform.position.y - mousePos.y, player.transform.position.x - mousePos.x) * Mathf.Rad2Deg;
-        spriteIndex = (int)Mathf.Abs(angle / 22.5f) % 4;
+        textureIndex = (int)Mathf.Abs(angle / 20f) % 8;
 
-        if (angle > -105f && angle < 105f) {
-            sprite = (Sprite)cursorSprites[spriteIndex];
-            sRenderer.flipX = false;
+        if (angle > -20f && angle < 160f) {
+            texture = (Texture2D)cursorTextures[textureIndex];
         } else {
-            sprite = (Sprite)cursorSprites[3 - spriteIndex]; // Subtract 3 to reverse the sprite index when on the opposite side of the player y-axis.
-            sRenderer.flipX = true;
-        }
-        
-        if (angle > 0)
-            sRenderer.flipY = false;
-        else
-            sRenderer.flipY = true;
-
-        cursorSprite.transform.rotation = Quaternion.Euler(0, 0, 0);
-        if (Mathf.Abs(angle) < 105 && Mathf.Abs(angle) > 90) { // Rotate the 0 degree sprite to 90 or -90 degrees when needed.
-            if (angle < 0) {
-                cursorSprite.transform.rotation = Quaternion.Euler(0, 0, -90);
-            } else {
-                cursorSprite.transform.rotation = Quaternion.Euler(0, 0, 90);
-            }
+            texture = (Texture2D)cursorTexturesFlipped[textureIndex];
         }
 
-        sRenderer.sprite = sprite;
+        Cursor.SetCursor(texture, Vector2.zero, CursorMode.ForceSoftware);
     }
 
 }
