b324b9743d511a2a6c1b3af77aa6e1ebc6b67061
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2021-07-10 09:20:09 -0700

    Update Player Movement
    Made speed scale inversely to mouse distance from player

--- SevenToHeaven/Assets/Scripts/PlayerMovement.cs
+++ SevenToHeaven/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,10 @@ public class PlayerMovement : MonoBehaviour {
 
     private Vector3 mousePos;
     private Vector2 direction;
+    private Vector2 magnitude;
+
+    public float xBound = 4f;
+    public float yBound = 4f;
 
     public Rigidbody2D rb2d { get; private set; }
     public bool windIsBlowing { get; private set; }
@@ -30,14 +34,30 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetMouseButton(0)) {
             windIsBlowing = true;
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction = (mousePos - transform.position).normalized;
+            direction = mousePos - transform.position;
+            float x = Mathf.Abs(direction.x);
+            float y = Mathf.Abs(direction.y);
+            direction = direction.normalized;
+            
+            if (x > xBound) {
+                x = xBound;
+                windIsBlowing = false;
+            }
+            if (y > yBound) {
+                y = yBound;
+                windIsBlowing = false;
+            }
+            x = xBound - x;
+            y = yBound - y;
+            magnitude = new Vector2(x,y);
+
         } else
             windIsBlowing = false;
     }
 
     private void FixedUpdate() {
         if (windIsBlowing) {
-            rb2d.velocity = new Vector2(direction.x * windStrength, direction.y * windStrength) * -1;
+            rb2d.velocity = new Vector2(direction.x * windStrength * magnitude.x, direction.y * windStrength * magnitude.y) * -1;
         }
     }
 }
